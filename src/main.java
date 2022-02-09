6288ee27e94c648b0bc8793a45cf607061fbd5ae
Loïc <loicmartinez3000@gmail.com>
2022-02-09 10:20:42 +0100

    first commit

---
+++ src/main.java
@@ -0,0 +1,39 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.io.IOException;  // Import the IOException class to handle errors
+
+public class main {
+    public static void main(String[] args) throws FileNotFoundException {
+        File index = new File("index.html");
+        try {
+            if (index.createNewFile()) {
+                System.out.println("File created: " + index.getName());
+            }
+            System.out.println("patch : " + index.getAbsolutePath());
+            Scanner obj = new Scanner(index);
+            while (obj.hasNextLine()) {
+                System.out.println(obj.nextLine());
+            }
+            PrintWriter writer = new PrintWriter("index.html");
+            writer.println("""
+                    <html>
+                        <head>
+                            <title>Welcome to Portail Agents!</title>
+                        </head>
+                        <body>
+                            <h1>Portail des agents </h1>
+                        </body>
+                    </html>
+                    """);
+            writer.close();
+
+            System.out.println("Fini");
+        }
+        catch(IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}
